adbb3fd6a1f6b2ca8c51f574ccbadd9ebd85aa89
elnurxo <elnurykh@code.edu.az>
2022-06-09 20:34:53 +0400

    9 june practise code

---
+++ 9JunePractise/9JunePractise/Extensions/ExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9JunePractise.Extensions
+{
+    static class ExtensionMethods
+    {
+        public static string ReverseWord(this string word)
+        {
+            string reversedword = "";
+
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+                reversedword += word[i];
+            }
+
+            return reversedword;
+        }
+
+       public static int[] AddElement(this int[] nums,int num)
+        {
+            Array.Resize(ref nums, nums.Length + 1);
+            nums[nums.Length - 1] = num;
+
+            return nums;
+        }
+    }
+}
---
+++ 9JunePractise/9JunePractise/Program.cs
@@ -0,0 +1,25 @@
+﻿using _9JunePractise.Extensions;
+using System;
+
+namespace _9JunePractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Abdulla";
+
+            //Console.WriteLine(name.ReverseWord());
+
+            int[] numbers = { 1,2,3};
+
+            numbers = numbers.AddElement(4);
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
